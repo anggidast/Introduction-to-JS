e430370e8f6b6ce01fc48851505e27d74306b49d
anggidast <adastariana@gmail.com>
2021-01-12 21:52:31 +0700

    VCube.js dengan function di dalam function

--- VCube.js
+++ VCube.js
@@ -1,15 +1,16 @@
+function volumeKubus(a) {
+    var v = a ** 3;
+
+    return v;
+}
+
 function jumlahVolumeDuaKubus(a, b) {
-    var vA;
-    var vB;
-    var total;
-
-    vA = a ** 3;
-    vB = b ** 3;
-
-    total = vA + vB;
+    var total = a + b;
 
     return total;
 }
 
-console.log(jumlahVolumeDuaKubus(8, 3));
-console.log(jumlahVolumeDuaKubus(10, 15));
+var a = 8;
+var b = 3;
+var total = jumlahVolumeDuaKubus(volumeKubus(a), volumeKubus(b));
+console.log(total);
