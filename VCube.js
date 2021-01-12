8bbb1d72dcc6512052e30328b3842f981addc671
anggidast <adastariana@gmail.com>
2021-01-12 21:57:29 +0700

    VCube.js refactoring sederhana

--- VCube.js
+++ VCube.js
@@ -1,16 +1,5 @@
-function volumeKubus(a) {
-    var v = a ** 3;
-
-    return v;
-}
-
 function jumlahVolumeDuaKubus(a, b) {
-    var total = a + b;
-
-    return total;
+    return a ** 3 + b ** 3;;
 }
 
-var a = 8;
-var b = 3;
-var total = jumlahVolumeDuaKubus(volumeKubus(a), volumeKubus(b));
-console.log(total);
+console.log(jumlahVolumeDuaKubus(8, 3));
